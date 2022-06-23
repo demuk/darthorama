609b864b2ab0041fabdb7efcac880c068e2ca2ea
Kiptoo Dennis <denysmutay@gmail.com>
2022-06-23 16:08:06 +0600

    added templates and static files

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template, redirect, url_for
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
