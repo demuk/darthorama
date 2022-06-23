479166e84fbb4f2c5d25d8b6a44ccf0513d82d72
kiptoo <denysmutay@gmail.com>
2022-06-24 01:07:17 +0300

    added route for sign in

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,22 @@ from app import app
 from flask import render_template, redirect, url_for
 
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	
+	return render_template('signin.html')
+
+
 @app.route('/contact')
 def contact():
+
     return render_template('contact.html')
 
 
