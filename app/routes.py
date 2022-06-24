30b2536062fe4465c57ae08b5aeafe79f487892e
Kiptoo Dennis <denysmutay@gmail.com>
2022-06-24 17:27:30 +0600

    added routes for signup and logout

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,48 @@
 from app import app
-from flask import render_template, redirect, url_for
-
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.security import check_password_hash
 
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
     return render_template('home.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	
-	return render_template('signin.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user:
+            if check_password_hash(user.password_hash, request.form['password']):
+                login_user(user, remember=True)
+            return redirect(url_for('home'))
+        flash('invalid Username or Password')
+        return redirect(url_for('login'))
+    return render_template('signin.html')
 
 
 @app.route('/contact')
 def contact():
-
     return render_template('contact.html')
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
