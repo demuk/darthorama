56bcd5c0be094c2ac6b5bc6367d18c2fb53711bd
Kiptoo Dennis <denysmutay@gmail.com>
2022-07-30 23:44:04 +0300

    initialized flask mail

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
 from flask_ckeditor import CKEditor
+from flask_mail import Message, Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,5 +15,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 moment = Moment(app)
 login.login_view = 'login'
+mail = Mail(app)
+
 
 from app import routes, models
